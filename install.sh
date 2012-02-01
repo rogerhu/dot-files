e68bac3df42c57015f8e6405224a80bd25584110
Adam DePue <adepue@Adams-MacBook-Pro.local>
2012-02-01 11:59:02 -0800

    install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+function link_file {
+    source="${PWD}/$1"
+    target="${HOME}/${1/_/.}"
+
+    if [ -e "${target}" ]; then
+        mv $target $target.bak
+    fi
+
+    ln -sf ${source} ${target}
+}
+
+link_file .bash_aliases
+link_file .bashrc
+link_file .vimrc
